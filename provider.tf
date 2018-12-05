0665669f41dae5582e6d3556a4c474df76ed6d4e
raviteja59 <yvraviteja@gmail.com>
2018-12-05 11:52:09 -0800

    push

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("./creds/tf-gke.json")}"
+  credentials = "${file("./creds/gke-tf-demo-224611-daefe9b7a4e6.json")}"
   project     = "gke-tf-demo"
   region      = "us-west1"
 }
